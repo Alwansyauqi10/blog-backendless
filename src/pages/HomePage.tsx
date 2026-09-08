a4e44816799eb5df15b7ef46917e09255fda3799
Alwansyauqi10 <alwansyauqi10@gmail.com>
2026-09-08 10:43:54 +0700

    commit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -36,6 +36,9 @@ function HomePage() {
             <Button variant="destructive" onClick={logout}>
               Logout
             </Button>
+            <Link to="/write">
+              <Button>Create Blog</Button>
+            </Link>
           </div>
         ) : (
           <div>
